1890a39b4e85bb7c03fa6b42cbc4a2eff95aa4c1
Amal chelbi <amal.chelbi@ealpha4tech.com>
2024-03-13 14:22:42 +0100

    Add crud for drink detail

--- drinks/views.py
+++ drinks/views.py
@@ -23,3 +23,28 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+
+    try:
+        # pk : primary key
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinksSerializer(drink)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = DrinksSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
